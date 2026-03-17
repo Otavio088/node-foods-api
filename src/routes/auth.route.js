ae91befa3a6c912ad3db032e8d50f892a0e5ffb9
Otavio088 <otavio208augustoribeiro@gmail.com>
2026-03-17 11:24:30 -0300

    add checks user auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const authMiddleware = require('../middlewares/auth.middleware');
 const authValidators = require('../validators/auth.validator');
 const authController = require('../controllers/auth.controller');
 
+router.get('/user', authMiddleware, (req, res) => { res.json({ user: req.user }); });
 router.post('/login', authValidators.login, authController.login);
 
 module.exports = router;
